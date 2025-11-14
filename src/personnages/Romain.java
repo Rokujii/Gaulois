1d13dae7ce1c160a0e8f6f60f4acfc04837ad6e4
Rokujii <tortuedeter@gmail.com>
2025-11-14 16:08:03 +0100

    Ajout invariant et variant dans la classe Romain

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -8,6 +8,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified() : "Invariant rompu : force négative après construction !";
 	}
 
 	public String getNom() {
@@ -23,14 +24,24 @@ public class Romain {
 	} 
 
 	public void recevoircoup(int force) {
+		assert force >= 0 : "Invariant rompu : force négative après construction !";
+		int forceTemp = this.force;
 		this.force = this.force - force ;
 		if (this.force <1) {
 			parler("J'abandonne !");			
 		}
 		else {
 			parler("Aïe !");
-		}		
+		}
+		assert isInvariantVerified() : "Invariant rompu : force négative après construction !";
+		assert forceTemp > this.force;
+	}
+	
+	private Boolean isInvariantVerified() {
+		return this.force >= 0;
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus",6);
 	}
 }
-
-
